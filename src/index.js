2a58d4887940c08a5d5c6173b841b3a884d3815a
高目维 <gaomuwei@qianmi.com>
2019-08-04 13:33:36 +0800

    taro调整

--- src/index.js
+++ src/index.js
@@ -50,10 +50,10 @@ module.exports = function(layoutData, options) {
             innerText = json.innerText;
           }
 
-          result += `<Text style={styles.${className}} numberOfLines={${lines}}>${innerText}</Text>`;
+          result += `<Text className='${className}' >${innerText}</Text>`;
 
           if (!raxImport[type]) {
-            raxImport[type] = `import Text from 'rax-text';`;
+            raxImport[type] = `import { Text } from '@tarojs/components';`;
           }
 
           if (json.style.lines == 1) {
@@ -66,14 +66,14 @@ module.exports = function(layoutData, options) {
           break;
         case 'view':
           if (json.children && json.children.length > 0) {
-            result += `<View style={styles.${className}}>${json2jsx(
+            result += `<View className='${className}'>${json2jsx(
               json.children
             )}</View>`;
           } else {
-            result += `<View style={styles.${className}} />`;
+            result += `<View className='${className}'/>`;
           }
           if (!raxImport[type]) {
-            raxImport[type] = `import View from 'rax-view';`;
+            raxImport[type] = `import { View } from '@tarojs/components';`;
           }
           break;
         case 'picture':
@@ -85,14 +85,15 @@ module.exports = function(layoutData, options) {
           } else {
             source = `'${json.attrs.src}'`;
           }
-          result += `<Picture resizeMod={'contain'} style={styles.${className}} source={{uri: ${source}}} />`;
+          result += `<Image className='${className}' src='${source}' />`;
 
           if (!raxImport[type]) {
-            raxImport[type] = `import Picture from 'rax-picture';`;
+            raxImport[type] = `import { Image } from '@tarojs/components';`;
           }
           break;
       }
 
+      // 构建样式数组
       style[className] = json.style;
     } else {
       return json
@@ -130,8 +131,22 @@ module.exports = function(layoutData, options) {
     })
     .join('\n');
   renderData.mockData = `var mock = ${JSON.stringify(mock)}`;
-  renderData.style = `var styles = ${JSON.stringify(style)}`;
   renderData.export = `render(<Mod dataSource={mock} />);`;
+
+  // renderData.style = `var styles = ${JSON.stringify(style)}`;
+  let stylesStr = '';
+  for (let styleName in style) {
+    let styleStr = '';
+    for (let className in style[styleName]) {
+      const classNameLow = camelTranslate(className);
+      const classValue = valueTranslate(classNameLow, style[styleName][className]);
+      let classStr = `${classNameLow}: ${classValue};`;
+      styleStr = `${styleStr}${classStr}`;
+    }
+    styleStr = `.${styleName} {${styleStr}}`;
+    stylesStr = `${stylesStr}\n${styleStr}`;
+  }
+  renderData.style = stylesStr;
 
   const prettierOpt = {
     printWidth: 120,
@@ -141,7 +156,90 @@ module.exports = function(layoutData, options) {
   return {
     renderData: renderData,
     xml: prettier.format(jsx, prettierOpt),
-    style: prettier.format(renderData.style, prettierOpt),
+    style: renderData.style,
+    // style: prettier.format(renderData.style, prettierOpt),
     prettierOpt: prettierOpt
   };
 };
+
+function camelTranslate(str) {
+  return str.replace(/([A-Z])/g, '-$1').toLowerCase();
+}
+
+function valueTranslate(classNameLow, classValue) {
+  switch (classNameLow) {
+    case 'font-size':
+    case 'margin-left':
+    case 'margin-top':
+    case 'margin-right':
+    case 'margin-bottom':
+    case 'padding-left':
+    case 'padding-top':
+    case 'padding-right':
+    case 'padding-bottom':
+    case 'width':
+    case 'height':
+    case 'border-radius':
+    case 'top':
+    case 'left':
+    case 'right':
+    case 'bottom':
+    case 'line-height':
+    case 'max-width':
+    case 'border-width':
+    case 'border-top-width':
+    case 'border-right-width':
+    case 'border-bottom-width':
+    case 'border-left-width':
+    case 'border-bottom-right-radius':
+    case 'border-bottom-left-radius':
+    case 'border-top-right-radius':
+    case 'border-top-left-radius':
+      if (typeof classValue == 'number') {
+        classValue = classValue + 'px';
+      } else if (typeof classValue == 'string') {
+        classValue = classValue.replace(/(px)|(rem)/, '');
+        classValue = classValue + 'px';
+      }
+      break;
+    case 'color':
+    case 'background-color':
+      let rgb = classValue.match(
+        /^rgb[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i
+      );
+      let rgba = /^rgba\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(.+))?\)$/.exec(
+        classValue
+      );
+      if (rgb && rgb.length === 4) {
+        classValue =
+          '#' +
+          ('0' + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+          ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+          ('0' + parseInt(rgb[3], 10).toString(16)).slice(-2);
+      }
+      if (rgba && rgba[4]) {
+        if (Number(rgba[4]) === 1) {
+          classValue =
+            '#' +
+            ('0' + parseInt(rgba[1], 10).toString(16)).slice(-2) +
+            ('0' + parseInt(rgba[2], 10).toString(16)).slice(-2) +
+            ('0' + parseInt(rgba[3], 10).toString(16)).slice(-2);
+        } else {
+          classValue =
+            'rgba(' +
+            rgba[1] +
+            ',' +
+            rgba[2] +
+            ',' +
+            rgba[3] +
+            ',' +
+            Number(rgba[4]).toFixed(2) +
+            ')';
+        }
+      }
+      break;
+    default:
+      break;
+  }
+  return classValue;
+}
